8d7097ac686805db6695acc3a9919b8dc903b3fe
Ariel Sanchez <arii.sanchez123@gmail.com>
2020-03-27 00:59:10 -0300

    Navbar
    Completados los estilos de la barra de navegacion.
    Menu dinamico.

--- scripts/1-navbar.js
+++ scripts/1-navbar.js
@@ -1,19 +1,34 @@
 let theme_menu = document.querySelectorAll('.drop-js');
+let link_outline = document.querySelectorAll('.out-js');
 let theme_select = document.querySelector('.theme-select').style;
 
 var check_menu = true;
-
-
 
 function open_close() {
     if (check_menu == true) {
         theme_select.visibility = 'visible';
         theme_select.top = '38px';
+
+        theme_menu.forEach(element => {
+            element.style.background = '#E6BBE2';
+        });
+
+        link_outline.forEach(element => {
+            element.style.outline = '1px dotted #110038';
+        });
         check_menu = false;
 
     } else {
         theme_select.visibility = 'hidden';
         theme_select.top = '15px';
+
+        theme_menu.forEach(element => {
+            element.removeAttribute('style');
+        });
+
+        link_outline.forEach(element => {
+            element.removeAttribute('style');
+        });
         check_menu = true;
     }
 }
