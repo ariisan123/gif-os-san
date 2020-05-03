173d62f565710e501e4e11667cf98a826cf5856c
Ariel Sanchez <arii.sanchez123@gmail.com>
2020-05-03 16:19:35 -0300

    dfdfg

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,46 +1,76 @@
-function newGif() {
-  const video = document.querySelector(".video-up");
-
-  video.autoplay = true;
+/* function newGif() {
   const constraints = navigator.mediaDevices.getUserMedia({
     audio: false,
-    video: { facingMode: "user" },
+    video: { facingMode: "user"},
   });
-
-  constraints.then((MediaStream) => {
-    video.srcObject = MediaStream;
+  
+  constraints.then((videoStream) => {
+    video.srcObject = videoStream;
+    video.play();
   });
-}
+} */
+const video = document.querySelector(".video-up");
 
 const start = document.querySelector("#start-btn");
-const recordButton = document.querySelector(".video-btn");
-
-function recordGif() {
-  let record = RecordRTC(stream, {
-    type: "gif",
-    frameRate: 30,
-    quality: 10,
-    onGifRecordingStarted: function () {
-      console.log("started");
-    },
-  }
-  record.startRecording()
-  );
-}
+const recordButton = document.querySelector(".video-buttons");
+const stopButton = document.querySelector(".stop-btn");
+const stopContainer = document.querySelector(".stop-container");
+const stopTimer = document.querySelector(".stop-timer");
 
 start.addEventListener("click", () => {
-  newElement(".new-main", "div", "video-container");
-  newElement(".video-container", "span", "video-title");
-  document.querySelector(".video-title").innerText =
-    "Un Chequeo Antes de Empezar";
-  newElement(".video-container", "div", "video-div");
-  newElement(".video-div", "video", "video-up");
-  newElement(".video-container", "div", "video-buttons");
-  newElement(".video-buttons", "span", "video-span");
-  newElement(".video-buttons", "button", "video-btn");
-  document.querySelector(".video-btn").innerText = "Capturar";
+  let stream = navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      width: 1280,
+      height: 720,
+    },
+  });
+
+  stream.then((videoStream) => {
+    video.srcObject = videoStream;
+    video.play();
+
+    let record = new RecordRTC(videoStream, {
+      type: "video",
+      frameRate: "60",
+      recorderType: MediaStreamRecorder,
+      width: 640,
+      height: 360,
+      quality: 10,
+      timeSlice: 1000,
+      onTimeStamp: function (time, timeArray) {
+        let duration = new Date().getTime() - timeArray[0] / 1000;
+
+        if (duration < 0) return;
+        stopTimer.innerHTML = duration;
+      },
+    });
+
+    recordButton.addEventListener("click", () => {
+      record.startRecording();
+      document.querySelector(".video-title").innerText = "Capturando Tu Guifo";
+      recordButton.style.display = "none";
+      stopContainer.style.display = "flex";
+    });
+
+    stopButton.addEventListener("click", () => {
+      record.stopRecording(() => {
+        let blob = record.getBlob();
+        video.srcObject = null;
+        video.src = URL.createObjectURL(blob);
+        video.controls = true;
+      });
+    });
+  });
 
   document.querySelector(".new-gif").style.display = "none";
-
-  newGif();
+  document.querySelector(".video-container").style.display = "flex";
 });
+
+/* recordButton.addEventListener("click", () => {
+  let record = new RecordRTC(stream)
+
+  document.querySelector(".video-title").innerText = "Capturando Tu Guifo";
+  recordButton.style.display = "none";
+  stopContainer.style.display = "flex";
+}); */
